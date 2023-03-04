374b2d80ea8d0c87cfe2ca1051e65f0aea9d8d69
HadesHappy <happyhades123@gmail.com>
2023-03-05 00:40:09 +0900

    z-index

--- src/components/landing/social.tsx
+++ src/components/landing/social.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 const Media = ({ name, icon, link }: Props) => {
   return (
-    <a href={`${link}`} target='_blank'>
+    <a href={`${link}`} target='_blank' className='z-10'>
       <div className="text-[#AFAFAF] bg-[#0F0F10] md:w-40 md:h-32 w-32 h-28 flex flex-col items-center justify-center gap-5 rounded-2xl z-10 cursor-pointer">
         {icon}
         <div>
@@ -36,10 +36,10 @@ const Social = () => {
         </div>
         <div className="mx-auto">
           <div className="flex md:flex-row flex-wrap items-center justify-center gap-10 md:py-32 py-10">
-            <Media name='Twitter' icon={<FaTwitter className='w-10 h-10' />} link="https://twitter.com/lsderivatives"/>
-            <Media name='Discord' icon={<FaDiscord className='w-10 h-10' />} link="https://discord.com/invite/lsderivatives" />
+            <Media name='Twitter' icon={<FaTwitter className='w-10 h-10' />} link="https://twitter.com/lsderivatives/"/>
+            <Media name='Discord' icon={<FaDiscord className='w-10 h-10' />} link="https://discord.com/invite/lsderivatives/" />
             <Media name='Medium' icon={<BsMedium className='w-10 h-10' />} link="https://lsderivatives.medium.com/"/>
-            <Media name='Github' icon={<FaGithub className='w-10 h-10' />} link="https://github.com/lsderivatives"/>
+            <Media name='Github' icon={<FaGithub className='w-10 h-10' />} link="https://github.com/lsderivatives/"/>
             <Media name='Coinmarketcap' icon={<img className='w-10 h-10' src='/assets/coinmarketcap.png' alt='coinmarketcap' />} link="https://www.lsdprotocol.io/#" />
             <Media name='Coingecko' icon={<img className='w-10 h-10' src='/assets/coingecko.png' alt='coingecko' />} link="https://www.coingecko.com/en/coins/liquid-staking-derivative"/>
           </div>

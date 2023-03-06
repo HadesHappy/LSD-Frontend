f0a1fd368fbc995e954ae3d683dc904d391a0c88
kevinorris <stevengerrard.mu@gmail.com>
2023-03-06 01:24:55 +0100

    mobile-fix

--- src/components/stake/main.tsx
+++ src/components/stake/main.tsx
@@ -1,6 +1,6 @@
 const Main = () => {
   return (
-    <div className="vector min-h-[25vh] bg-[#050505]">
+    <div className="vector bg-[#050505]">
       <div className="mx-auto text-center">
         <div className="sm:w-[400px] h-[500px]">
           <img className="pt-32 -ml-20" src="/assets/dots.png" alt="dots" />
